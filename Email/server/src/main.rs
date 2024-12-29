96a8babeda5bb12401d11df0b716eced1507ffa8
Max Lee <maximusalee@gmail.com>
2024-12-29 15:15:49 -0800

    initial rough-in of smtp

--- Email/server/src/main.rs
+++ Email/server/src/main.rs
@@ -1,3 +1,62 @@
-fn main() {
-    println!("Hello, world!");
+use std::net::{TcpListener, TcpStream};
+use std::io::{Read, Write, BufReader, BufRead};
+use std::thread;
+use std::time::Duration;
+use std::str;
+
+use server::smtp;
+
+const PORT: &str = "2525";
+const DOMAIN: &str = "test.ourdomain.org";
+const BACKLOG_MAX: usize = 10;
+const BUF_SIZE: usize = 4096;
+
+fn handle_smtp(mut stream: TcpStream) -> std::io::Result<()> {
+    println!("Starting thread for connection from {}", stream.peer_addr()?);
+
+    let mut buffer = [0; BUF_SIZE];
+    let mut inmessage = false;
+
+    let mut bufferout = format!("220 {} SMTP CCSMTP\r\n", DOMAIN);
+    print!("{}", bufferout);
+    stream.write_all(bufferout.as_bytes())?;
+    let mut smtp_object = smtp::SMTPStateMachine::default();
+    smtp_object.in_message = false;
+
+    loop {
+        stream.set_read_timeout(Some(Duration::from_secs(120)))?;
+
+        let bytes_read = stream.read(&mut buffer)?;
+        if bytes_read == 0 {
+            println!("Remote host closed socket");
+            break;
+        }
+
+        let _result = smtp_object.load_buffer(&buffer[..bytes_read]);
+
+        let _result = smtp_object
+    }
+
+    Ok(())
+}
+
+fn main() -> std::io::Result<()> {
+    let listener = TcpListener::bind(format!("0.0.0.0:{}", PORT))?;
+
+    for stream in listener.incoming() {
+        match stream {
+            Ok(stream) => {
+                thread::spawn(move || {
+                    if let Err(e) = handle_smtp(stream) {
+                        eprintln!("Error handling connection: {}", e);
+                    }
+                });
+            }
+            Err(e) => {
+                eprintln!("Connection failed: {}", e);
+            }
+        }
+    }
+
+    Ok(())
 }
--- Email/server/src/smtp.rs
+++ Email/server/src/smtp.rs
@@ -1,4 +1,4 @@
-use std::io::Read;
+use std::io::{BufRead, BufReader, Read};
 
 //
 //
@@ -12,7 +12,7 @@ struct SMTPObject {
     attachments: Vec<Vec<u8>>
 }
 
-#[derive(Default, Debug)]
+#[derive(Default, Debug, PartialEq, Eq)]
 enum SMTPState {
     #[default] StartingState,
     DATA, // Respond with 354 if ok
@@ -34,10 +34,12 @@ enum SMTPState {
 }
 
 #[derive(Default, Debug)]
-struct SMTPStateMachine {
+pub struct SMTPStateMachine {
     input_buffer: Vec<u8>, // This approach may be inefficient (taking data copied out of pipe), but allows for testing.
+    output_buffer: Vec<u8>, // We then need to write this to stream
     mail_object: SMTPObject,
-    current_state: SMTPState
+    current_state: SMTPState,
+    pub in_message: bool
 }
 
 impl SMTPStateMachine {
@@ -52,17 +54,136 @@ impl SMTPStateMachine {
         Ok(())
     }
 
-    pub fn check_state(&mut self) {
-        if self.current_state == SMTPState::DATA {
-            // Check for solo "." character
-            if self.input_buffer[0] == b'.' {
+    pub fn check_for_state_change(&mut self) -> Result<(), Box<dyn std::error::Error>> {
+        let received = std::str::from_utf8(&self.input_buffer).unwrap_or("");
+        let mut reader = BufReader::new(received.as_bytes());
+        for line_result in reader.lines() {
+            let line = line_result?;
+            if !self.in_message {
+                println!("C: {}", line);
 
+                let command = line.to_uppercase();
+                let parts: Vec<&str> = command.split_whitespace().collect();
+
+                match parts[0] {
+                    "HELO" => { bufferout = "250 Ok\r\n".to_string(); },
+                    "MAIL" => { bufferout = "250 Ok\r\n".to_string(); },
+                    "RCPT" => { bufferout = "250 Ok recipient\r\n".to_string(); },
+                    "DATA" => { bufferout = "354 Continue\r\n".to_string(); self.in_message = true;},
+                    "RSET" => { bufferout = "250 Ok reset\r\n".to_string();},
+                    "NOOP" => { bufferout = "250 Ok noop\r\n".to_string(); },
+                    "QUIT" => { bufferout = "221 Ok\r\n".to_string(); stream.write_all(bufferout.as_bytes())?; break; },
+                    _ => { bufferout = "502 Command Not Implemented\r\n".to_string(); },
+                }
+                print!("S: {}", bufferout);
+                stream.write_all(bufferout.as_bytes())?;
+                bufferout.clear();
+            } else {
+                // We're still in the middle of a DATA command and reading in bytes.
+                println!("C: {}", line);
+                if line == "." {
+                    bufferout = "250 Ok\r\n".to_string();
+                    print!("S: {}", bufferout);
+                    stream.write_all(bufferout.as_bytes())?;
+                    self.in_message = false;
+                }
             }
         }
+
+        match self.current_state {
+            SMTPState::StartingState => todo!(),
+            SMTPState::DATA => {
+                if self.input_buffer[0] == b'.' && self.input_buffer.len() == 1 {
+                    self.current_state = SMTPState::StartingState
+                } else {
+
+                }
+            },
+            SMTPState::BDAT => todo!(),
+            SMTPState::NOOP => todo!(),
+            SMTPState::HELP => todo!(),
+            SMTPState::HELO => todo!(),
+            SMTPState::EHLO => todo!(),
+            SMTPState::MAILFROM => todo!(),
+            SMTPState::RCPTTO => todo!(),
+            SMTPState::RSET => todo!(),
+            SMTPState::QUIT => todo!(),
+            SMTPState::AUTH => todo!(),
+            SMTPState::STARTTLS => todo!(),
+            SMTPState::ATRN => todo!(),
+            SMTPState::VRFY => todo!(),
+            SMTPState::EXPN => todo!(),
+            SMTPState::AUTHLOGIN => todo!(),
+        }
     }
+
+    pub fn handle_current_state(&mut self) -> Result<(), Box<dyn std::error::Error>> {
+        let received = std::str::from_utf8(&self.input_buffer).unwrap_or("");
+        let mut reader = BufReader::new(received.as_bytes());
+        for line_result in reader.lines() {
+            let line = line_result?;
+            if !self.in_message {
+                println!("C: {}", line);
+
+                let command = line.to_uppercase();
+                let parts: Vec<&str> = command.split_whitespace().collect();
+
+                match parts[0] {
+                    "HELO" => { bufferout = "250 Ok\r\n".to_string(); },
+                    "MAIL" => { bufferout = "250 Ok\r\n".to_string(); },
+                    "RCPT" => { bufferout = "250 Ok recipient\r\n".to_string(); },
+                    "DATA" => { bufferout = "354 Continue\r\n".to_string(); self.in_message = true;},
+                    "RSET" => { bufferout = "250 Ok reset\r\n".to_string();},
+                    "NOOP" => { bufferout = "250 Ok noop\r\n".to_string(); },
+                    "QUIT" => { bufferout = "221 Ok\r\n".to_string(); stream.write_all(bufferout.as_bytes())?; break; },
+                    _ => { bufferout = "502 Command Not Implemented\r\n".to_string(); },
+                }
+                print!("S: {}", bufferout);
+                stream.write_all(bufferout.as_bytes())?;
+                bufferout.clear();
+            } else {
+                // We're still in the middle of a DATA command and reading in bytes.
+                println!("C: {}", line);
+                if line == "." {
+                    bufferout = "250 Ok\r\n".to_string();
+                    print!("S: {}", bufferout);
+                    stream.write_all(bufferout.as_bytes())?;
+                    self.in_message = false;
+                }
+            }
+        }
+
+        match self.current_state {
+            SMTPState::StartingState => todo!(),
+            SMTPState::DATA => {
+                if self.input_buffer[0] == b'.' && self.input_buffer.len() == 1 {
+                    self.current_state = SMTPState::StartingState
+                } else {
+
+                }
+            },
+            SMTPState::BDAT => todo!(),
+            SMTPState::NOOP => todo!(),
+            SMTPState::HELP => todo!(),
+            SMTPState::HELO => todo!(),
+            SMTPState::EHLO => todo!(),
+            SMTPState::MAILFROM => todo!(),
+            SMTPState::RCPTTO => todo!(),
+            SMTPState::RSET => todo!(),
+            SMTPState::QUIT => todo!(),
+            SMTPState::AUTH => todo!(),
+            SMTPState::STARTTLS => todo!(),
+            SMTPState::ATRN => todo!(),
+            SMTPState::VRFY => todo!(),
+            SMTPState::EXPN => todo!(),
+            SMTPState::AUTHLOGIN => todo!(),
+        }
+    }
+
 }
 
 
-enum StateError {
+pub enum StateError {
 
 }
+
